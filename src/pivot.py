aa88147488a6fb9e2d9510477eb6b75cc8e27e06
parsa-salimi <parsa2000kurd@gmail.com>
2021-04-10 18:52:38 -0400

    steepest edge

--- src/pivot.py
+++ src/pivot.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import time
 import timeit
+import math
 
 def timer(fxn):
     def timed_fxn(self, *arg, **kw):
@@ -44,4 +45,23 @@ class RandomRule(PivotRule):
                 negative_indices.append(j)            
 
         return random.choice(negative_indices)
+
+class SteepestEdge(PivotRule):
+    def computePivotIndex(self, reduced_costs, A):
+        chosen_j = 0
+        chosen_steepness = 0
+        for j in reduced_costs.keys():
+            if reduced_costs[j] < 0.0:
+                colj = A[:j]
+                column_squared = np.array([x**2 for x in colj])
+                accum = np.sum(column_squared)
+                denom = math.sqrt(1 + accum)
+                steepness = (reduced_costs[j] / denom)
+                if (steepness < chosen_steepness):
+                    chosen_steepness = steepness
+                    chosen_j = j
+        return chosen_j
+
+
+
 
